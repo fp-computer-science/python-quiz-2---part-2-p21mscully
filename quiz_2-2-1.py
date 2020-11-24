f97a2daddb952c839c913db8ca5b466260dbce20
Matthew Scully <p21mscully@fairfieldprep.org>
2020-11-24 13:30:15 -0500

    Quiz 2

---
+++ quiz_2-2-1.py
@@ -0,0 +1,12 @@
+# Author MRS 11/24/20
+
+print(input("What Letter Day is it? "))
+
+my_list = ['A', 'a', 'C', 'c', 'E', 'e']
+
+school = input
+
+if school == my_list:
+    print("You Have class")
+else:
+    print("You Don't class")
---
+++ quiz_2-2-2.py
@@ -0,0 +1,10 @@
+# Author MRS 11/24/20
+
+print(input("Type something either positive "))
+
+word = input
+
+if 'not' in word
+    print("You're positive")
+else:
+    print("You're negative")
